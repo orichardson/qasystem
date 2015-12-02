f736c692be57912e7bc97160bdbd8f06eb1b5d8c
Chris <cbrooks90@gmail.com>
2015-12-01 17:06:03 -0700

    working

--- libutils.py
+++ libutils.py
@@ -21,7 +21,7 @@ def isplural(word):
     lemma = wnl.lemmatize(word, 'n')
     plural = True if word is not lemma else False
     return plural, lemma
-    
+
 def lexclass(word, context=''):
     # TODO: word sense disambiguation, pass optional context to lexclass.
     rslt = set()
@@ -37,28 +37,28 @@ def lexclass(word, context=''):
         rslt.add(s.lexname())
 
     return rslt
-    
+
 def bagSimilarity(s1, s2) :
     total = 0;
-    
+
     for a in s1:
         for b in s2:
             total += wn.lch_similarity(a,b)
-            
+
     total /= (len(s1)*len(s2))
     return total
-    
+
 def semanticDist(texts1, texts2):
     s1 = set()
     s2 = set()
-    
+
     for t in texts1:
         for q in t.split():
             s1 |= lexclass(q, t)
     for t in texts2:
         for q in t.split():
             s2 |= lexclass(q, t)
-        
+
     return bagSimilarity(s1, s2)
 
 
@@ -67,13 +67,13 @@ def traverse(t, f, pre=False):
         # Now we know that t.node is defined
         if pre:
             f(t)
-            
+
         for child in t:
             traverse(child,f)
-        
+
         if not pre:
             f(t)
-            
+
 def findLeftNode(t) :
     if isinstance(t, nltk.Tree):
         if len(t) :
@@ -81,21 +81,21 @@ def findLeftNode(t) :
             return n if n else t
         return t
     return None
-    
+
 def ismatch(ltags, node) :
     return ' '.join(ltags) is ' '.join([x.label()  for x in node if isinstance(x,nltk.Tree)])
-    
+
 def getMatches(grammar, node):
     matches = set()
-    
+
     for g in grammar.keys():
         # risky business here: use in, or is? Do we need exact match?
         if ismatch(grammar[g], node) :
             matches.add(g)
-    
+
     return matches
 
-    
+
 
 def parse(string):
     split = [ s.replace('\n', ' ') for s in nltk.sent_tokenize(string)]
@@ -109,26 +109,26 @@ def parse(string):
             trees.append(nltk.ParentedTree.fromstring(tree.pformat()))
 
     return trees;
-    
+
 
 
 def maketagger(labels):
     labels.reverse()
-    
+
     def nertagger(node) :
         l = node.label()
 
         if len(node) == 1 and isinstance(node[0], str) :
             # We are now checking that labels.pop()[0] == node[0]
             newlabel = labels.pop();
-            
+
             if(l == newlabel[0]) :
                 node.nertag = newlabel[1];
-                
+
         elif l.startswith('N') :
             # Do the last one, not the most common one. Head nouns.
             node.nertag = getattr(node[-1], 'nertag', 'O')
-            
+
     return nertagger
 
 """
@@ -151,27 +151,45 @@ def levenshtein(s1, s2) :
             substitutions = previous_row[j] + (c1 != c2)
             current_row.append(min(insertions, deletions, substitutions))
         previous_row = current_row
-    
+
     return previous_row[-1]
-    
+
 """
-... and this is why we need it. 
+... and this is why we need it.
 Returns lexical similarity between two strings (0-1)
 Edit distance + Acronyms + other ways of co-referring to strings
 where semantics and syntax don't help at all (using just first names, etc.)
 """
+
+def acronimify(s):
+    s.replace('.', '')
+    s.replace('of', '')
+    s.replace('and', '')
+    s.split()
+    s = [i[0].lower() for i in s]
+
+def isacronym(s1, s2):
+    s1.replace('.', '')
+
+
 def lexsim(s1, s2):
     dist = levenshtein(s1,s2)
 
+
     #Now do fancy acronym and missing word / reordering things to add score.
     amelioration = 2 # should be greater for reorderings, less for egregious mistakes
-    
+
+    '''if isacronym(s1,s2):
+        amelioration = 1000
+    if issubphrase(s1,s2):
+        amelioration = 100'''
+
     # and now to clamp it to the appropriate range:
     return 1/(1 + dist/amelioration)
-    
 
-    
-    
+
+
+
 #trees = []
 #for i in parse('I am a sentence at Apple, Co.'):
 #    for j in i:
